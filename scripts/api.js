321490cd4dbd250ff4b3b5e39e5d348e0f7d90db
Omar <omared250@gmail.com>
2021-08-20 23:24:52 +0200

    api

---
+++ scripts/api.js
@@ -0,0 +1,20 @@
+async function getAllPokemon() {
+    const url = "https://pokeapi.co/api/v2/pokemon/?limit=20";
+
+    const response = await fetch(url);
+    const { results } = await response.json();
+    return results;
+}
+
+async function getOnePokemonSprite (url) {
+
+    const response = await fetch(url);
+    const { sprites } = await response.json();
+    return sprites.front_shiny;
+    //console.log("Here I will be returning a sprite from  my Pokemon");
+
+    // fetch the response from url
+    // return one of the sprites we get
+}
+
+export { getAllPokemon, getOnePokemonSprite };
